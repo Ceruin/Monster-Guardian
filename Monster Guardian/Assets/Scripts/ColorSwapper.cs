2ce0670b93c16953253ecc1852140005d336ff3f
Ceruin <13532471+Ceruin@users.noreply.github.com>
2021-06-29 02:10:35 -0400

    Updates
    > Project Version Upgrade
    > Scene Setup

---
+++ Monster Guardian/Assets/Scripts/ColorSwapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwapper : MonoBehaviour
+{
+    public void SetRed() {
+        Debug.Log(":");
+        GameObject test = GameObject.Find("Pet");
+        test.transform.localScale = new Vector3(2,1,1);
+    }
+
+    public void SetBlu() {
+        Debug.Log(":");
+        GameObject test = GameObject.Find("Pet");
+        test.transform.localScale = new Vector3(1,2,1);
+    }
+
+    public void SetGrn() {
+        Debug.Log(":");
+        GameObject test = GameObject.Find("Pet");
+        test.transform.localScale = new Vector3(1,1,2);
+    }
+}
