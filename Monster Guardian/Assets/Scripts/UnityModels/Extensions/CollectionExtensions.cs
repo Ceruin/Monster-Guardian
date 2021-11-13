ce3e26cc9a6fb1bddcfc7237183516e04e105673
Ceruin <13532471+Ceruin@users.noreply.github.com>
2021-11-13 16:40:51 -0500

    Code

--- Monster Guardian/Assets/Scripts/UnityModels/Extensions/CollectionExtensions.cs
+++ Monster Guardian/Assets/Scripts/UnityModels/Extensions/CollectionExtensions.cs
@@ -7,12 +7,12 @@ using UnityEngine;
 
 public static class  CollectionExtensions
 {
-    public static string ToJson<T>(ICollection<T> collection)
+    public static string ToJson<T>(this ICollection<T> collection)
     {
         return JsonUtility.ToJson(collection);
     }
 
-    public static string ToJson<T>(ICollection<T> collection, bool prettyPrint)
+    public static string ToJson<T>(this ICollection<T> collection, bool prettyPrint)
     {
         return JsonUtility.ToJson(collection, prettyPrint);
     }
